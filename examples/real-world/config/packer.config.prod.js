11419f885b03e74fdfd591ea4ca0ee8383271ff2
zhbhun <zhbhun@gmail.com>
2017-02-24 12:45:54 +0800

    change real-world production configuration

--- examples/real-world/config/packer.config.prod.js
+++ examples/real-world/config/packer.config.prod.js
@@ -13,6 +13,7 @@ module.exports = {
   output: {
     path: {
       build: 'build',
+      publicPath: '',
     },
   },
   chunks: [
--- examples/real-world/src/Root.prod.js
+++ examples/real-world/src/Root.prod.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Router, browserHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 
 import routes from './routes';
 
 const Root = () => (
-  <Router history={browserHistory} routes={routes} />
+  <Router history={hashHistory} routes={routes} />
 )
 
 export default Root;
