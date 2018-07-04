2c76ccc2ed45549895e1a46989feef6cbf7ad1fc
zhbhun <zhbhun@gmail.com>
2018-07-04 13:17:54 +0800

    Bump version to 4.2.2
    1. 修正 ejs 不兼容 webpack 4 的问题
    2. 解决热加载跨域问题

--- src/scripts/start.js
+++ src/scripts/start.js
@@ -54,6 +54,11 @@ const makeConfig = (rawConfig) => {
               overlay: true,
               quiet: true,
               useLocalIp: false,
+              headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+                'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+              },
             },
           },
           rawConfig,
